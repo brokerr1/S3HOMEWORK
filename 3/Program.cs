cc93095ce8133dd228aecebbc79ea7ca46c53268
brokerr1 <solovievdima1@gmail.com>
2022-05-17 10:51:50 +0300

    3.3

---
+++ 3/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая принимает на
+вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 4, 9.
+5 -> 1, 8, 27, 64, 125
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int step = 1;
+Console.WriteLine("Таблица квадратов чисел: ");
+Console.WriteLine("Число|Число в кубе");
+while (step < (number+1))
+{
+    Console.WriteLine($"    {step}|{step*step*step}");
+    step++;
+}
